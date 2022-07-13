01d7d10c603fe45e8eebcc50edd20f704e81b464
Brian Yoon <infobyoon@gmail.com>
2022-07-12 18:11:32 -0700

    fix ethereum-waffle and @nomicfoundation/hardhat-chai-matchers bug

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,9 +1,9 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('@nomicfoundation/hardhat-chai-matchers');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
 	solidity: '0.8.3',
-
 	paths: {
 		artifacts: './src/artifacts',
 	},
